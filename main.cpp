61e23a72a3ba6968c75d7b18a55b0fba9deed9f0
lkarus <duyloc_1503@kaist.ac.kr>
2020-04-07 15:43:51 +0900

    Add initial files

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int minus(int x, int y) {
+  return x - y;
+}
+
+int plus(int x, int y) {
+  return x + y;
+}
+
+bool compare(int x, int y) {
+  if (x > y)
+    return 1;
+  else if (x < y)
+    return -1;
+  else
+    return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+  return 0;
+}
+
