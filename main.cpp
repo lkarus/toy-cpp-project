970b336d7099a0f6b879315b587bcc7f90c193c9
lkarus <duyloc_1503@kaist.ac.kr>
2020-04-07 16:06:48 +0900

    Minor update

--- main.cpp
+++ main.cpp
@@ -4,17 +4,15 @@ int minus(int x, int y) {
   return x - y;
 }
 
-int plus(int x, int y) {
-  return x + y;
+int multiply(int x, int y) {
+  return x * y;
 }
 
 bool compare(int x, int y) {
-  if (x > y)
+  if (x > y || x == y)
     return 1;
-  else if (x < y)
+  else 
     return -1;
-  else
-    return 0;
 }
 
 int main(int argc, char const *argv[])
